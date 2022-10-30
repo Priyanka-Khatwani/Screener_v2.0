7b649a1dbfbb4ae87bb27bb387224fb971d94dd5
Priyanka-Khatwani <105270721+Priyanka-Khatwani@users.noreply.github.com>
2022-10-30 15:39:18 +0530

    Update Engulfing, swing, pinbar & bar.py

--- pages/Engulfing, swing, pinbar & bar.py
+++ pages/Engulfing, swing, pinbar & bar.py
@@ -2,14 +2,15 @@ import pandas as pd
 import yfinance as yf
 import streamlit as st
 
-st.title("Patterns for 5 minutes interval")
+st.title("Candlestick patterns identification for Nifty100")
 sample=['ACC.NS','ADANIENT.NS','ADANIGREEN.NS','ADANIPORTS.NS','ATGL.NS','ADANITRANS.NS','AMBUJACEM.NS','APOLLOHOSP.NS','ASIANPAINT.NS','DMART.NS','AXISBANK.NS','BAJAJ-AUTO.NS','BAJFINANCE.NS','BAJAJFINSV.NS','BAJAJHLDNG.NS','BANDHANBNK.NS','BANKBARODA.NS','BERGEPAINT.NS','BEL.NS','BPCL.NS','BHARTIARTL.NS','BIOCON.NS','BOSCHLTD.NS','BRITANNIA.NS','CHOLAFIN.NS','CIPLA.NS','COALINDIA.NS','COLPAL.NS','DLF.NS','DABUR.NS','DIVISLAB.NS','DRREDDY.NS','EICHERMOT.NS','NYKAA.NS','GAIL.NS','GLAND.NS','GODREJCP.NS','GRASIM.NS','HCLTECH.NS','HDFCAMC.NS','HDFCBANK.NS','HDFCLIFE.NS','HAVELLS.NS','HEROMOTOCO.NS','HINDALCO.NS','HAL.NS','HINDUNILVR.NS','HDFC.NS','ICICIBANK.NS','ICICIGI.NS','ICICIPRULI.NS','ITC.NS','IOC.NS','IRCTC.NS','INDUSTOWER.NS','INDUSINDBK.NS','NAUKRI.NS','INFY.NS','INDIGO.NS','JSWSTEEL.NS','KOTAKBANK.NS','LTI.NS','LT.NS','LICI.NS','M&M.NS','MARICO.NS','MARUTI.NS','MPHASIS.NS','MUTHOOTFIN.NS','NTPC.NS','NESTLEIND.NS','ONGC.NS','PAYTM.NS','PIIND.NS','PIDILITIND.NS','POWERGRID.NS','PGHH.NS','RELIANCE.NS','SBICARD.NS','SBILIFE.NS','SRF.NS','MOTHERSON.NS','SHREECEM.NS','SIEMENS.NS','SBIN.NS','SUNPHARMA.NS','TCS.NS','TATACONSUM.NS','TATAMOTORS.NS','TATAPOWER.NS','TATASTEEL.NS','TECHM.NS','TITAN.NS','TORNTPHARM.NS','UPL.NS','ULTRACEMCO.NS','MCDOWELL-N.NS','VEDL.NS','WIPRO.NS','ZOMATO.NS']
 
 indicators=("Bullish engulfing","Bearish engulfing","Bullish Swing","Bearish swing","Bullish pinbar","Bearish pinbar","Inside bar","Outside bar")
 indicator=st.selectbox(label="Choose an indicator you're most interested in",options=indicators)
 name=st.selectbox(label="Choose the stock you're most interested in",options=sample)
+interval=st.selectbox(label="Interval Preference",options=('1m','2m','5m','15m','30m','60m','90m','1d','5d','1wk')
 
-df=yf.download(tickers=name,period='22d',interval='5m')
+df=yf.download(tickers=name,period='22d',interval=interval)
 df.reset_index(inplace=True)
 
 for i in range(2,df.shape[0]):
