130bebe8e89f7b6cdad0255a159c31107c8bfdd4
Priyanka-Khatwani <105270721+Priyanka-Khatwani@users.noreply.github.com>
2022-10-30 14:42:54 +0530

    Update Engulfing, swing, pinbar & bar.py

--- pages/Engulfing, swing, pinbar & bar.py
+++ pages/Engulfing, swing, pinbar & bar.py
@@ -10,6 +10,7 @@ indicator=st.selectbox(label="Choose an indicator you're most interested in",opt
 name=st.selectbox(label="Choose the stock you're most interested in",options=sample)
 
 df=yf.download(tickers=name,period='22d',interval='5m')
+st.dataframe(df)
 
 for i in range(2,len(df)):
   current = df.iloc[i,:]
