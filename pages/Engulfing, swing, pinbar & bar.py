9eab682082bebd988a3e1c014435317a7c7698b0
Priyanka-Khatwani <105270721+Priyanka-Khatwani@users.noreply.github.com>
2022-10-30 15:28:39 +0530

    Update Engulfing, swing, pinbar & bar.py

--- pages/Engulfing, swing, pinbar & bar.py
+++ pages/Engulfing, swing, pinbar & bar.py
@@ -12,34 +12,34 @@ name=st.selectbox(label="Choose the stock you're most interested in",options=sam
 df=yf.download(tickers=name,period='22d',interval='5m')
 df.reset_index(inplace=True)
 
-for index, row in df.iterrows():
-      st.write(index)
-      st.write(row)
-      #current = df.iloc[i,:]
-      #prev = df.iloc[i-1,:]
-      #prev_2 = df.iloc[i-2,:]
-      #realbody = abs(current['Open'] - current['Close'])
-      #candle_range = current['High'] - current['Low']
-      #idx = df.index[i]
-
-      df.loc[idx,'Bullish engulfing'] = current['High'] > prev['High'] and current['Low'] < prev['Low'] and realbody >= 0.8 * candle_range and current['Close'] > current['Open'] #Bullish engulfing
-      df.loc[idx,'Bearish engulfing'] = current['High'] > prev['High'] and current['Low'] < prev['Low'] and realbody >= 0.8 * candle_range and current['Close'] < current['Open'] #Bearish engulfing
-      df.loc[idx,'Bullish swing'] = current['Low'] > prev['Low'] and prev['Low'] < prev_2['Low']  #Bullish Swing
-      df.loc[idx,'Bearish swing'] = current['High'] < prev['High'] and prev['High'] > prev_2['High'] #Bearish Swing
-      df.loc[idx,'Bullish pinbar'] = realbody <= candle_range/3 and  min(current['Open'], current['Close']) > (current['High'] + current['Low'])/2 and current['Low'] < prev['Low'] #Bullish pinbar
-      df.loc[idx,'Bearish pinbar'] = realbody <= candle_range/3 and max(current['Open'] , current['Close']) < (current['High'] + current['Low'])/2 and current['High'] > prev['High'] #Bearish pinbar
-      df.loc[idx,'Inside bar'] = current['High'] < prev['High'] and current['Low'] > prev['Low'] #Inside bar
-      df.loc[idx,'Outside bar'] = current['High'] > prev['High'] and current['Low'] < prev['Low'] #Outside bar
-      df.fillna(False, inplace=True)
-
-         
-      for types in range(0,len(indicators)):
-               if indicator==indicators[types]:
-                  df=df[df[indicator]==True]
-
-        
-            
-
+for i in range(2,df.shape[0]):
+  current = df.iloc[i,:]
+  prev = df.iloc[i-1,:]
+  prev_2 = df.iloc[i-2,:]
+  realbody = abs(current['Open'] - current['Close'])
+  candle_range = current['High'] - current['Low']
+  idx = df.index[i]
+  
+  # Bullish swing
+  df.loc[idx,'Bullish swing'] = current['Low'] > prev['Low'] and prev['Low'] < prev_2['Low']
+# Bearish swing
+  df.loc[idx,'Bearish swing'] = current['High'] < prev['High'] and prev['High'] > prev_2['High']
+# Bullish pinbar
+  df.loc[idx,'Bullish pinbar'] = realbody <= candle_range/3 and  min(current['Open'], current['Close']) > (current['High'] + current['Low'])/2 and current['Low'] < prev['Low']
+# Bearish pinbar
+  df.loc[idx,'Bearish pinbar'] = realbody <= candle_range/3 and max(current['Open'] , current['Close']) < (current['High'] + current['Low'])/2 and current['High'] > prev['High']
+    
+  # Inside bar
+  df.loc[idx,'Inside bar'] = current['High'] < prev['High'] and current['Low'] > prev['Low']
+    
+  # Outside bar
+  df.loc[idx,'Outside bar'] = current['High'] > prev['High'] and current['Low'] < prev['Low']
+  
+  # Bullish engulfing
+  df.loc[idx,'Bullish engulfing'] = current['High'] > prev['High'] and current['Low'] < prev['Low'] and realbody >= 0.8 * candle_range and current['Close'] > current['Open']
+# Bearish engulfing
+  df.loc[idx,'Bearish engulfing'] = current['High'] > prev['High'] and current['Low'] < prev['Low'] and realbody >= 0.8 * candle_range and current['Close'] < current['Open']
+  df.fillna(False, inplace=True)
 st.dataframe(df)
     
     
